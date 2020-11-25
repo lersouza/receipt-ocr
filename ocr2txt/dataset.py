54b71f05d3e5a1865e88fdd61ae0059510a65ae8
leandrors <leandrors@ciandt.com>
2020-11-25 10:56:08 -0300

    pre train model and scripts

--- ocr2txt/dataset.py
+++ ocr2txt/dataset.py
@@ -1,41 +1,102 @@
+import json
+import torch
+
 from pathlib import Path
+from typing import Union
+from PIL import Image
 
 from torch.utils.data import Dataset
 from torchvision.transforms import transforms
 
+from transformers import PreTrainedTokenizer, AutoTokenizer
+
 
 class WikiTextMaskedDataset(Dataset):
-    def __init__(self, dataset_dir: str, enhance_factor: int = 3) -> None:
+    """
+    Dataset for pre-training models on synthetic images based on WikiText.
+    """
+    DEFAULT_TRANSFORMS = transforms.Compose([
+        transforms.Resize((512, 256)),
+        transforms.ToTensor(),
+    ]) 
+    RANDOM_TRANSFORMS = transforms.RandomApply(torch.nn.ModuleList([
+        transforms.GaussianBlur(3),
+        transforms.ColorJitter(),
+        transforms.RandomRotation(degrees=(-5, 5)),
+    ]), p=0.5)
+
+    def __init__(self,
+                 dataset_dir: str,
+                 tokenizer: Union[str, PreTrainedTokenizer],
+                 target_max_length: int = 256,
+                 enhance_factor: int = 1) -> None:
+        """
+        Initializes the dataset:
+        - `dataset_dir`: Directory containing the dataset files (png, json and mask)
+        - `tokenizer`: A `PreTrainedTokenizer` or the name of one.
+        - `target_max_length`: The maximum length for a target text to be predicted.
+        - `enhance_factor`: A factor to scale the dataset.
+        """
+
         self.images = list(Path(dataset_dir).glob('**/*.png'))
         self.labels = list(Path(dataset_dir).glob('**/*.png.json'))
         self.masks = list(Path(dataset_dir).glob('**/*.png.mask'))
 
-        self.images, self.labels, self.masks = (
-            sorted(self.images),
-            sorted(self.labels),
-            sorted(self.masks))            
+        self.images, self.labels, self.masks = (sorted(self.images),
+                                                sorted(self.labels),
+                                                sorted(self.masks))
 
         self.enhance_factor = enhance_factor
         self.original_size = len(self.images)
 
-        assert len(self.images > 0)
+        assert len(self.images) > 0
         assert len(self.images) == len(self.labels) == len(self.masks)
-        assert self.images[0].stem == self.labels[0].stem == self.masks[0].stem
+        assert self.images[0].stem + '.png' == self.labels[0].stem == self.masks[0].stem
 
+        self.label_content = []
         self._load_labels()
 
+        self.tokenizer = (tokenizer if isinstance(tokenizer, PreTrainedTokenizer)
+                          else AutoTokenizer.from_pretrained(tokenizer))
+
+        self.target_max_length = target_max_length
+
     def __len__(self):
+        """ Returns the length of the dataset. """
         return len(self.images) * self.enhance_factor
 
     def __getitem__(self, index):
+        """
+        Returns a dict representing the sample located at `index`:
+
+        - `['image']`: a PIL image transformed by WikiTextMaskedDataset.DEFAULT_TRANSFORMS + RANDOM_TRANSFORMS.
+        - `['mask']`: CharGrid mask for the image.
+        - `['target']: A Target Text to be predicted, encoded by `self.tokenizer`.        
+        """
         actual_index = index % self.original_size
 
-        image, mask, label = (
-            self.images[actual_index], self.masks[actual_index],
-            self.labels[actual_index])
+        image, mask, label = (self.images[actual_index],
+                              self.masks[actual_index],
+                              self.label_content[actual_index])
 
-        mask = self.masks[index % self.original_size]
-        return None
+        image = Image.open(image)
+        image = self.RANDOM_TRANSFORMS(self.DEFAULT_TRANSFORMS(image))
+
+        mask = torch.load(mask).transpose(1, 0)
+
+        target_label = self.tokenizer(label['original'],
+                                      max_length=self.target_max_length,
+                                      padding='max_length',
+                                      truncation=True)
+
+        return {'image': image, 'mask': mask.long(),
+                'target': torch.tensor(target_label.input_ids, dtype=torch.long)}
 
     def _load_labels(self):
-        pass
+        """
+        Pre load labels, since they are the smallest files.
+        """
+        for label in self.labels:
+            with open(label, 'r') as label_file:
+                label_config = json.load(label_file)
+            self.label_content.append(label_config)
--- ocr2txt/models.py
+++ ocr2txt/models.py
@@ -1,109 +1,188 @@
+import string
 from typing import Tuple
+
 import pytorch_lightning as pl
+import torch
+import torch.nn.functional as F
 
+from torch import nn
 from torch.optim import Adam
-from torch import nn, Size
-from torch.tensor import Tensor
-
-from efficientnet_pytorch import EfficientNet
-from transformers import T5ForConditionalGeneration, T5Tokenizer
+from transformers import T5ForConditionalGeneration, T5Model
 
 
-class ConvolutionalBridge(nn.Module):
-    def __init__(self,
-                 encoder_output_size: Size,
-                 decoder_input_size: Size) -> None:
-        
-        self.encoder_output_size = encoder_output_size
-        self.decoder_input_size = decoder_input_size
-
-    def forward(self, images):
-        pass
-
-
-class EfficientNetEncoder(nn.Module):
-    def __init__(self,
-                 pretrained_model: str,
-                 in_channels: int = 3) -> None:
+class DoubleConvBlock(nn.Module):
+    """ Adapted from: https://github.com/hiepph/unet-lightning/blob/master/Unet.py """
+    def __init__(self, in_channels: int, out_channels: int) -> None:
         super().__init__()
 
-        self.net = EfficientNet.from_pretrained(pretrained_model,
-                                                in_channels=in_channels)
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
 
-        # TODO Make dynamic based on pretrained_model
-        self.output_size = (1280, 7, 7)  
-
-    def forward(self, images):
-        return self.net.extract_features(images)
+    def forward(self, image):
+        return self.block(image)
 
 
-class T5Decoder(nn.Module):
-    def __init__(self, pretrained_model: str) -> None:
+class UNetDownBlock(nn.Module):
+    """ Adapted from: https://github.com/hiepph/unet-lightning/blob/master/Unet.py """
+    def __init__(self, in_channels: int, out_channels: int) -> None:
         super().__init__()
 
-        self.t5 = T5ForConditionalGeneration.from_pretrained(pretrained_model)
-        self.tokenizer = T5Tokenizer.from_pretrained(pretrained_model)
+        self.block = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConvBlock(in_channels, out_channels)
+        )
 
-        self.input_size = (-1, self.t5.model_dim)  # Get Encoder Shape
-
-    def forward(self, encoder_inputs, targets = None):
-        if self.training:
-            assert targets is not None
-
-            output = self.t5(input_embeds=encoder_inputs,
-                             labels=targets,
-                             output_dict=True)
-            
-            return output.loss
-
-        return None  #  TODO: Implement generation method.
+    def forward(self, image):
+        return self.block(image)
 
 
-class Finetuner(pl.LightningModule):
-    def __init__(self,
-                 encoder: str,
-                 decoder: str,
-                 bridge: str,
-                 learning_rate: float,
-                 pretrained_encoder: str = None,
-                 pretrained_decoder: str = None,
-                 freeze_settings: Tuple[bool, bool, bool] = (False, False, False),
-                 **kwargs) -> None:
+class UNetUpBlock(nn.Module):
+    """ Extracted from: https://github.com/hiepph/unet-lightning/blob/master/Unet.py """
+    def __init__(self, in_channels: int, out_channels: int, bilinear: bool = True) -> None:
+        super().__init__()
+
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels // 2, in_channels // 2,
+                                         kernel_size=2, stride=2)
+
+        self.conv = DoubleConvBlock(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        x = torch.cat([x2, x1], dim=1) ## why 1?
+
+        return self.conv(x)
+
+
+class UNet(nn.Module):
+    """ Extracted from: https://github.com/hiepph/unet-lightning/blob/master/Unet.py """
+
+    def __init__(self, in_channels: int, out_classes: int) -> None:
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.out_classes = out_classes
+        self.bilinear = True
+
+        # UNet Arch
+        self.inc = DoubleConvBlock(self.in_channels, 64)
+
+        # ---> Down
+        self.down1 = UNetDownBlock(64, 128)
+        self.down2 = UNetDownBlock(128, 256)
+        self.down3 = UNetDownBlock(256, 512)
+        self.down4 = UNetDownBlock(512, 512)
+
+
+        # ---> Up
+        self.up1 = UNetUpBlock(1024, 256)
+        self.up2 = UNetUpBlock(512, 128)
+        self.up3 = UNetUpBlock(256, 64)
+        self.up4 = UNetUpBlock(128, 64)
+
+        # ---> Classifier
+        self.out = nn.Conv2d(6, self.out_classes, kernel_size=1)
+
+    def forward(self, image):
+        x1 = self.inc(image)
+
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+
+        return self.out(x)
+
+
+class EncoderPreTrainingModel(pl.LightningModule):
+    """ Checkout https://github.com/hiepph/unet-lightning/blob/master/Unet.py"""
+    def __init__(self, image_width: int, image_height: int, seq_length: int,
+                 t5_model: str, learning_rate: float):
         super().__init__()
 
         self.save_hyperparameters()
 
-        self.encoder = getattr(globals(), encoder)(pretrained_encoder, **kwargs)
-        self.decoder = getattr(globals(), decoder)(pretrained_decoder, **kwargs)
+        self.t5 = T5ForConditionalGeneration.from_pretrained(t5_model)
+        self.d_model = self.t5.config.d_model
 
-        self.bridge = getattr(globals(), bridge)(
-            self.encoder.output_size,
-            self.decoder.input_size)
+        self.encoder_classes = len(string.digits + string.ascii_letters) + 1  # Account for UNK
+        self.encoder = UNet(3, self.encoder_classes)
+        
+        self.encoder_expand = nn.Conv2d(self.encoder_classes, 128, kernel_size=1)
 
-        self.setup_layer(freeze_settings[0], self.encoder)
-        self.setup_layer(freeze_settings[1], self.bridge)
-        self.setup_layer(freeze_settings[2], self.decoder)
+        self.adapter = nn.Sequential(
+            nn.Linear(image_width * image_height, 2048),
+            nn.Linear(2048, 1024),
+            nn.Linear(1024, self.d_model))
 
-    def setup_layer(self, freeze: bool, layer: nn.Module):
-        if freeze is True:
-            for param in layer.parameters():
-                param.requires_grad = False
+    def forward(self, image):
+        """
+        Runs the model for the given image batch.
+        
+        `image` is of shape: (B, C, H, W):
+        - B: Batch Size
+        - C: Channels (RGB=3)
+        - H: Image Heights
+        - W: Image Widths.
+        
+        This returns 2 outputs:
+        - `[0]`: The logits after passing `image` throught U-Net. Shape: (B, Co, H, W)
+        - `[1]`: The logits after passing `[0]` through Embedding adapter. Shape: (B, W, D)
 
-    def forward(self, images: Tensor, targets: Tensor):
-        encoded = self.encoder(images)
-        bridge_output = self.bridge(encoded)
-        decoded = self.decoder(bridge_output, targets)
+        Co: The number of categories in the segmentation Task (#chars + #digits)
+        L: Sequence Length to be considered in this model (hparams.seq_length).
+        """
+        batch_size = image.shape[0]
 
-        return decoded
+        encoded = self.encoder(image)  # Shape: (B, Co, H, W)
+        
+        expanded = self.encoder_expand(encoded)  # Shape: (B, 128, H, W)
+        flat = expanded.view(batch_size, 128, -1)  # Shape: (B, Co, H, W)
+
+        adapted = self.adapter(flat)  # Shape: (B, 128, self.d_model)
+
+        return encoded, adapted
 
     def configure_optimizers(self):
         return Adam(self.parameters(), lr=self.hparams.learning_rate)
 
     def training_step(self, batch, batch_idx):
-        loss = self(batch['image'], batch['target'])
+        image_logits, adapted_emb = self(batch['image'])
+        image_expected = batch['mask']
 
-        self.log('train_loss', loss, on_epoch=True)
+        emb_expected = self.t5.get_input_embeddings()(batch['target'])  # Shape: (B, L, D)
+        emb_expected = torch.mean(emb_expected, dim=1)  # Shape: (B, D)
+
+        adapted_emb = torch.mean(adapted_emb, dim=1) # Shape: (B, D)
+
+        loss1 = F.cross_entropy(image_logits, image_expected)
+        loss2 = F.cosine_embedding_loss(adapted_emb, emb_expected, torch.ones(adapted_emb.shape[0]))
+
+        self.log('train_segmentation_loss', loss1, on_step=True)
+        self.log('train_embedding_loss', loss2, on_step=True)
+
+        loss = loss1 + loss2
+
+        self.log('train_loss', loss, on_step=True)
 
         return loss
 
-    
